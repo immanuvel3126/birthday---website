f7dd3ef537cbca8ff8ef416f5a20688fa2b88fcf
immanuvel3126 <immanuvel.jebastin26@gmail.com>
2026-07-25 20:48:29 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 // Month is 0-indexed: 0=Jan, 1=Feb, ..., 6=Jul, ..., 11=Dec
 // ============================================================
 const CONFIG = {
-    birthdayDate: new Date(2026, 6, 25, 13,40,0 ), // July 1, 2026, 12:00 AM
+    birthdayDate: new Date(2026, 6, 25, 08,57,0 ), // July 1, 2026, 12:00 AM
     birthdayGirl: 'kavya',
     createdBy: 'Immanuvel',
     maxPhotos: 10,
