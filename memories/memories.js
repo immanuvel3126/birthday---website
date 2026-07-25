4bb4220d0d5602000c01ed397bb5dd13062b1f53
immanuvel3126 <immanuvel.jebastin26@gmail.com>
2026-07-25 18:23:22 +0530

    Update memories.js

--- memories/memories.js
+++ memories/memories.js
@@ -18,19 +18,19 @@ const MEMORIES_DATA = [
     {
         date: "19 August 2025",
         title: "First Meeting",
-        description: "Namma clg vandha starting laa enakum unna yaarunu theriyaadhu unakum enna yaarunu theriyadhu aana namma ippa rpdi irukumla endha alavuku close saa oru semmayaana bond oda nalla iruku...anaa idha expext pannala dii🤭 adhuvum unta irundhu idha!!!! and enaku therinji idhan namma edutha first pic ninaikuren adhan idha poten",
+        description: "Namma clg vandha starting laa enakum unna yaarunu theriyaadhu unakum enna yaarunu theriyadhu aana namma ippa epdi irukumla endha alavuku close saa oru semmayaana bond oda nalla iruku...anaa idha expext pannala dii🤭 adhuvum unta irundhu idha!!!! and enaku therinji idhan namma edutha first pic ninaikuren adhan idha poten",
         image: "memories/first-meet.jpg"
     },
     {
         date: "13 january 2026",
         title: "Pongal",
-        description: "Pongal pathi romba perusa solradhuku illa but anniku nalla irundhuchi...nalla oru memory adhu nee alaga sarree la vandha oru maari nee bus la irundu vandhadhula irundhu unna admire pannen nyabagam iruka unaku....nariya pics laam eduthom annikum nalla irundhuchi💗 ",
+        description: "Pongal pathi romba perusa solradhuku illa but anniku nalla irundhuchi...nalla oru memory.... anniku nee alaga saree la vandha...oru maari nee bus la irundu erangii varum bodhula irundhu unna naa paathutu irundhen unaku nyabagam irukaaa dii....unna vandhadhula irundhu admire pannen nyabagam iruka unaku....nariya pics laam eduthom annikum nalla irundhuchi💗 ",
         image: "memories/pongal.jpg"
     },
     {
         date: "31 january 2026",
         title: "First out",
-        description: "Idha vandhutu ellamey un memory la irundhu eduthadhudiii. Na anniku oru naal ketrupen unta enga namma 1st time veliya ponom,ennlam paanom apdi ipdinu. Adhu ellamey idhuku dhaan.maybe nee enna idhukum thiruttu paiyan nu solva..ama naa apdi dhan😉...nammalo first padam adhu.And nee naa ippa pandramaari laam pannala anniku namm..yedho summa ukaandhutu irundhom... ana nee solladha onnu unaku thonadha onnu ippanaa solren paaru...annikey enaku oru maari jolly dha irundhuchi iva kuuda uakandhu serndhu padam paakurom pakkathula apdinu..and annikey nee padam paathutu irundhapa naa unna dhan fulla paathutu irudhen🫣(solladha theriyaadhaa unma idhu).apa nee naa mattum thaniya ukaandhu padam paathom nyabagam iruka..oru thani row laa thniya ukaandhu paathom🤭...apro anniki fulla unna nariya vaati thuukuren nu soltu irundhen nee vena vena nu soltu irundha and anniku namma pala naal ninachadha senjom pani puri saptom🤭..unna thaniya kupten nariya vaati..apro namma serndhu oru dress paathom oru bodycon maari semmya...apro naa asingama ooru dress kadaila onnu pannen and anikey semmaya vaangunen ippa varaikumey vaangitu dhaan iruken(sorry dii chello adhuku) ",
+        description: "Idha vandhutu ellamey un memory la irundhu eduthadhudiii. Na anniku oru naal ketrupen unta enga namma 1st time veliya ponom,ennalam paanom apdi ipdinu. Adhu ellamey idhuku dhaan.maybe nee enna idhukum thiruttu paiyan nu solva..ama naa apdi dhan😉...nammaloda first padam adhu...And nee naa ippa pandramaari laam pannala anniku namma...kai laam pudichikkala edhuvumeyy pannala summa pakkathula ukaandhu irundhom thaniya irundhom avlodhaan... ana naa solladha oru unmaiya vum thonadha onnum ippanaa solren paaru...annikey enaku oru maari jollyya dha irundhuchi iva kuuda ukaandhu serndhu padam paakurom pakkathula apdinu..and annikey nee padam paathutu irundhapa naa unna dhan fulla paathutu irudhen🫣(solladha theriyaadhaa unma idhu).apa nee naa mattum thaniya ukaandhu padam paathom nyabagam iruka..oru thani row laa thniya ukaandhu paathom🤭...apro anniki fulla unna nariya vaati thuukuren nu soltu irundhen nee vena vena nu soltu irundha and anniku namma pala naal ninachadha senjom pani puri saptom🤭..unna thaniya kupten nariya vaati..apro namma serndhu oru dress paathom oru bodycon maari semmya...apro naa asingama oru dress kadaila onnu pannen and anikey semmaya vaangunen ippa varaikumey vaangitu dhaan iruken(sorry dii chello adhuku) ",
         image: "memories/pheonixmall.jpg"
     },
     {
